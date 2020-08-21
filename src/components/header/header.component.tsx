ba5f09003303333d24d0cdaef96083bb900b6fc7
Andrew Lahikainen <andrew@lahikainen.co.nz>
2020-08-21 22:22:40 +1200

    change name header to a link returning to the index page

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import { BuildIcon } from '../icons/build'
-import { ChildCareIcon } from '../icons/child-care'
 import { FaceIcon } from '../icons/face'
 import styles from './header.module.css'
 
 export const Header = () => (
   <header className={styles.header}>
-    <h1 style={{ margin: 0 }}>Andrew Lahikainen</h1>
+    <Link className={styles.headingLink} to="/">
+      <h1 style={{ margin: 0 }}>Andrew Lahikainen</h1>
+    </Link>
     <span className="flex-spacer"></span>
     <nav className={`${styles.nav} body-1`}>
       <Link className={styles.navItem} activeClassName={styles.navItemActive} to="/blog/">
@@ -17,10 +18,6 @@ export const Header = () => (
       <Link className={styles.navItem} activeClassName={styles.navItemActive} to="/tools/">
         <BuildIcon fill="white" className={styles.navItemIcon}></BuildIcon>
         Tools
-      </Link>
-      <Link className={styles.navItem} activeClassName={styles.navItemActive} to="/me/">
-        <ChildCareIcon fill="white" className={styles.navItemIcon}></ChildCareIcon>
-        About me
       </Link>
     </nav>
   </header>
