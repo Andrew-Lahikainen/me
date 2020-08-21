c8ca1243d17771722dfa2f7e947c586ec8653fa8
Andrew Lahikainen <andrew@lahikainen.co.nz>
2020-08-21 22:22:57 +1200

    add tools page

---
+++ src/pages/tools/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/layout/layout'
+import SEO from '../../components/seo'
+
+const ToolsPage = () => {
+  return (
+    <Layout centered={true}>
+      <SEO title="Tools"></SEO>
+      <h1>Tools</h1>
+    </Layout>
+  )
+}
+
+export default ToolsPage
